0abc169d8eef52db6f4528991c1300e6582aa8d1
Rodrigo Vaz <rodrigodp2014@gmail.com>
2024-05-14 21:57:23 -0300

    feat: add DateOnly methods

---
+++ src/Library/Extensions/DateOnly.cs
@@ -0,0 +1,30 @@
+﻿namespace Library.Extensions;
+
+public static class DateOnlyExtension
+{
+    /// <summary>
+    /// Calculate difference days between two dates
+    /// </summary>
+    /// <param name="dateOnly">Greater date</param>
+    /// <param name="dateSubtract">Less date</param>
+    /// <returns>int with number of days</returns>
+    public static int DaysDifference(this DateOnly dateOnly, DateOnly dateSubtract) =>
+        dateOnly.DayNumber - dateSubtract.DayNumber;
+    
+    /// <summary>
+    /// Calculate aproximated years with difference of days with parameter
+    /// </summary>
+    /// <param name="dateOnly">Number os days between two dates</param>
+    /// <returns>int with numbers of years</returns>
+    public static int YearsDifference(this DateOnly dateOnly, int differeceDays) =>
+        differeceDays / 365;
+    
+    /// <summary>
+    /// Calculate difference between two dates
+    /// </summary>
+    /// <param name="dateOnly">Greater date</param>
+    /// <param name="dateSubtract">Less date</param>
+    /// <returns>int with numbers of years</returns>
+    public static int CalculateDiferrence(this DateOnly dateOnly, DateOnly dateSubtract) => 
+        dateOnly.YearsDifference(dateOnly.DaysDifference(dateSubtract));
+}
---
+++ tests/Library.Test/Extensions.Test/DateOnlyExtensionTest.cs
@@ -0,0 +1,43 @@
+﻿using Library.Extensions;
+
+namespace Library.Test.Extensions.Test;
+
+public class DateOnlyExtensionTest
+{
+    [Fact]
+    [Trait("DateOnly", "Difference Between Dates")]
+    public void Should_Be_Calculate_Difference_Between_Dates()
+    {
+        var dateOne = new DateOnly(1995, 08, 30);
+        var dateTwo = new DateOnly(2024, 08, 30);
+
+        var difference = dateTwo.DaysDifference(dateOne);
+
+        var equals = difference.Equals(10593);
+        equals.Should().BeTrue();
+    }
+    
+    [Fact]
+    [Trait("DateOnly", "Difference Between Years")]
+    public void Should_Be_Calculate_Difference_Between_Years()
+    {
+        var date = new DateOnly(1995, 08, 30);
+        var difference = date.YearsDifference(10593);
+
+        var equals = difference.Equals(29);
+        equals.Should().BeTrue();
+    }
+    
+    [Fact]
+    [Trait("DateOnly", "Difference Between Years With CalculateDiferrence Method")]
+    public void Should_Be_Calculate_Difference_Between_Years_With_CalculateDiferrence()
+    {
+        var dateOne = new DateOnly(1995, 08, 30);
+        var dateTwo = new DateOnly(2024, 08, 30);
+
+        var difference = dateTwo.CalculateDiferrence(dateOne);
+        
+        var equals = difference.Equals(29);
+        equals.Should().BeTrue();
+    }
+}
